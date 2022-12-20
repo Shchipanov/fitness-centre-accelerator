2d66c519d2ef0432f11f7df10d9a0211bbc743db
Shchipanov <chips_80@mail.ru>
2022-12-20 14:55:06 +0400

    устраняет замечания код-ревью

--- source/js/modules/init-video.js
+++ source/js/modules/init-video.js
@@ -1,19 +1,27 @@
+import {video} from '../../media/video';
+
 export const initVideo = function () {
+  const player = document.querySelector('[data-video-player]');
+  const link = document.querySelector('[data-video-btn]');
+  const boxVideo = document.querySelector('[data-video]');
 
-  let player = document.querySelector('[data-video-player]');
-  let link = document.querySelector('[data-video-btn]');
-  let boxVideo = document.querySelector('[data-video]');
+  if (link) {
+    link.addEventListener('click', (evt) => {
+      evt.preventDefault();
 
-  link.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    boxVideo.classList.add('gum__video--active');
-    // eslint-disable-next-line no-new, no-undef
-    new YT.Player(player, {
-      videoId: '9TZXsZItgdw',
-      events: {
-        onReady: (e) => e.target.playVideo(),
-      },
+      if (boxVideo) {
+        boxVideo.classList.add('gum__video--active');
+
+        // eslint-disable-next-line no-new, no-undef
+        new YT.Player(player, {
+          videoId: '9TZXsZItgdw',
+          events: {
+            onReady: (e) => e.target.playVideo(),
+          },
+          video,
+        });
+        evt.stopPropagation();
+      }
     });
-    evt.stopPropagation();
-  });
+  }
 };
